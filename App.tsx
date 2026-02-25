1b41e93915cb67921b188d0f4cf8f23e90af5327
SUPRØ <00youareworngtho00@gmail.com>
2026-02-25 18:16:45 +0530

    feat(app): add immersive mode to auto hide android nav buttons

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { 
 	StatusBar,
 	useColorScheme, 
 } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ImmersiveMode from 'react-native-immersive-mode';
 
 import ResistorScreen from "@/screens/resistor-screen";
 import ComponentsScreen from '@/screens/components-screen';
 
 const App = () => {
 	const isDarkMode = useColorScheme() === 'dark';
+	useEffect(() => {
+		ImmersiveMode.fullLayout(true);
+		ImmersiveMode.setBarMode("BottomSticky");
+		return () => {
+            ImmersiveMode.setBarMode("Normal");
+		}
+	});
+
 	return(
 		<SafeAreaProvider>
 			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
